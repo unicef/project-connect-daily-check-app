c96e3b258325d0ff7aa5438193f94dbd2cd8e0d1
ezmonka <kajal.mondol@ericsson.com>
2022-01-20 16:33:12 +0530

    ionic installed

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'unicef-pdca',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
