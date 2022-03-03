63da9d46f21d816fe371d9106a486cde2a35eb8a
efijkoc <s.janakiraman@ericsson.com>
2022-03-03 10:04:12 +0530

    daily check changes

--- src/app/schoolsuccess/schoolsuccess.page.ts
+++ src/app/schoolsuccess/schoolsuccess.page.ts
@@ -3,6 +3,8 @@ import { IonAccordionGroup } from '@ionic/angular';
 import { ActivatedRoute, Router } from "@angular/router";
 import { SchoolService } from '../services/school.service';
 import { LoadingService } from '../services/loading.service';
+import { IonSlides} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-schoolsuccess',
@@ -11,15 +13,19 @@ import { LoadingService } from '../services/loading.service';
 })
 export class SchoolsuccessPage {
   @ViewChild(IonAccordionGroup, { static: true }) accordionGroup: IonAccordionGroup;
+  @ViewChild('mySlider')slides: IonSlides;
   schools: any;
   schoolId: any;
   private sub: any;
   constructor(
     private activatedroute: ActivatedRoute, 
-    public loading: LoadingService,
+    public loading: LoadingService,    
     private schoolService: SchoolService) {
     this.sub = this.activatedroute.params.subscribe(params => {
       this.schoolId = params.schoolId;
     });    
+  }
+  swipeNext(){
+    this.slides.slideNext();
   }
 }
