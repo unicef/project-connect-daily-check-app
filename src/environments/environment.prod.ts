865cf1e86f0bf4c028be4dbc8997b1d906f5bb0a
sanoylab <expertsanoy@gmail.com>
2023-10-19 18:43:10 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   restAPI: 'https://uni-connect-services.azurewebsites.net/api/v1/',
   //restAPI: 'http://localhost:3000/api/v1/',
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImRhaWx5Y2hlY2thcHAiLCJpYXQiOjE1MTYyMzkwMjJ9.9lHxUZ0XmSc-5ddqEEKFt2Opx2CC-gSsRTGSCI-KcQU',
+  token: '',
   app_version: '1.0.4'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
  // restAPI: 'https://uni-connect-services.azurewebsites.net/api/v1/',
   restAPI: 'https://uni-connect-services.azurewebsites.net/api/v1/',
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImRhaWx5Y2hlY2thcHAiLCJpYXQiOjE1MTYyMzkwMjJ9.9lHxUZ0XmSc-5ddqEEKFt2Opx2CC-gSsRTGSCI-KcQU',
+  token: '',
   app_version: '1.0.4'
 };
 
