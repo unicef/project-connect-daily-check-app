e0c5969da5516949c50343bc2699166e627f9a1a
Aditya Acharya <aditya.acharya@nagarro.com>
2024-01-15 13:41:02 +0530

    update dca rest API references

--- src/app/auth/token.interceptor.ts
+++ src/app/auth/token.interceptor.ts
@@ -3,32 +3,52 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-    constructor() {}
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (environment.token && this.isHeaderNeeded(request.url)){
-            request = request.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${environment.token}`
-                }
-            });
-        }
-        return next.handle(request);
+  constructor() {}
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (environment.token && this.isHeaderNeeded(request.url)) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${environment.token}`,
+        },
+      });
+    } else if (environment.dcaToken && this.isDcaHeaderNeeded(request.url)) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${environment.dcaToken}`,
+        },
+      });
     }
-//   request.url.indexOf('some APIs path') === 0
+    return next.handle(request);
+  }
+  //   request.url.indexOf('some APIs path') === 0
 
-    isHeaderNeeded(url: string) {
-        console.log(url.indexOf(environment.restAPI));
-        if (url.indexOf(environment.restAPI) === -1) { // this condition is up to you, it could be an exact match or how ever you like
-            return false;
-        } else {
-            return true;
-        }
+  isHeaderNeeded(url: string) {
+    console.log(url.indexOf(environment.restAPI));
+    if (url.indexOf(environment.restAPI) === -1) {
+      // this condition is up to you, it could be an exact match or how ever you like
+      return false;
+    } else {
+      return true;
     }
+  }
+
+  isDcaHeaderNeeded(url: string) {
+    console.log(url.indexOf(environment.dcaRestAPI));
+    if (url.indexOf(environment.dcaRestAPI) === -1) {
+      // this condition is up to you, it could be an exact match or how ever you like
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
