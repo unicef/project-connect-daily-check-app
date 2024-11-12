cbed42cf5e7554c0142962c16af910a5a1f7bc71
Victor Lopez <viclopez@unicef.com>
2024-11-12 16:39:16 +0100

    refactor: Update token assignment in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   mode: env.mode,
   // restAPI: 'https://uni-connect-services.azurewebsites.net/api/v1/',
   restAPI: env.mode === 'dev' ? env.restAPIDev : env.restAPI,
-  token,
+  token: env.mode === 'dev' ? env.tokenDev : token,
   app_version: '1.0.10',
   appName: 'Project Connect',
   appNameSuffix: 'Daily Check',
